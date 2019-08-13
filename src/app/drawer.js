f8e4bf35d3bc63047f2d2e02e6643163d60efd12
Jt Porter <jtporter9@gmail.com>
2019-08-13 13:06:22 -0500

    finished up main nav, and landing page for desktop, added favicon

--- src/app/drawer.js
+++ src/app/drawer.js
@@ -108,41 +108,25 @@ export default function PermanentDrawerLeft() {
         <List>
           {['About', 'Experience', 'Projects', 'Skills', 'Awards', 'Education', 'Contact', 'Resume'].map((text, index) => (
             <ListItem button key={text}>
-              <ListItemIcon><Icon className={clsx(classes.icon, icons[index].icon)} style={{color: icons[index].color}} /></ListItemIcon>
+              <ListItemIcon><Icon className={clsx(classes.icon, icons[index].icon)} style={{ color: icons[index].color }} /></ListItemIcon>
               <ListItemText primary={text} />
             </ListItem>
           ))}
-          {console.log(icons)}
-          {/* <ListItem button key="About">
-            <ListItemIcon><Icon className={clsx(classes.icon, "fa fa-user")} color="primary" /></ListItemIcon>
-            <ListItemText primary="About" />
-          </ListItem> */}
         </List>
       </Drawer>
       <main>
-        <div className="overlay"></div>
-        <Container className="hero-container">
-          <h2 className="hero-text">
-            I want to <span className="teal">make things</span> <br /> that <span className="underline">make a difference.</span>
-          </h2>
-        </Container>
-        {/* <Grid container spacing={3}>
-          <Grid item xs={12}>
-          </Grid>
-        </Grid> */}
-        {/* Content goes here . . . */}
-        {/* <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-          donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-          Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-          imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-          donec massa sapien faucibus et molestie ac.
-        </Typography> */}
+        <section id="intro">
+          <div className="overlay"></div>
+          <Container className="hero-container">
+            <h2 className="hero-text">
+              I want to <span className="teal">make things</span> <br /> that <span className="underline">make a difference.</span>
+            </h2>
+          </Container>
+        </section>
+        <section>
+          TESTING.
+        </section>
+
       </main>
     </div>
   );
