c29d5edd2213e81afb4e2c8388ce16eeab017b25
JT Porter <jtporter9@gmail.com>
2019-11-20 20:37:57 -0600

    added resume to mega because of github security issue

--- src/app/drawer/drawer.js
+++ src/app/drawer/drawer.js
@@ -86,7 +86,7 @@ export default function PermanentDrawerLeft() {
     {
       icon: 'fa fa-file',
       color: '#424242',
-      anchor: 'https://docs.google.com/document/d/1RlUb7U_8oEDtm-BiAnOWgN2nOYUBqSrpQm0sKx0aMXI/edit?usp=sharing',
+      anchor: 'https://mega.nz/#!mI8zUKLS!ImVVY79dw25KJFzWEEuUsMwAVxrl7hfqubOXrsgS41U',
       target: "_blank",
       rel: "noopener noreferrer"
     }
