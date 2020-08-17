cbc5ba8a9dbd2726c878cfdf9b05710648dbf9d0
JT Porter <jtporter9@gmail.com>
2020-08-16 21:06:43 -0600

    changed hero

--- src/app/sections/intro/intro.js
+++ src/app/sections/intro/intro.js
@@ -8,6 +8,8 @@ export default function Intro() {
         <section id="intro" name="intro">
           <div className="overlay"></div>
           <div className="hero-container">
+            <h1 className="hero-text mobile">JT Porter</h1>
+            <div><span className="hero-text mobile" style={{fontSize: "24px"}}>Full Stack Developer</span></div>
             <h2 className="hero-text">
               I want to <span className="teal">make things</span> <br /> that <span className="underline">make a difference.</span>
             </h2>
